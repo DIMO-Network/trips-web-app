2d081dc0dd3f050044a6169b2663edc99f70e93c
Laura Deng <laura@dimo.zone>
2024-02-28 16:00:14 -0600

    added an alert for bug

--- api/get_device_status.go
+++ api/get_device_status.go
@@ -3,8 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/rs/zerolog/log"
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 
 	"github.com/dimo-network/trips-web-app/api/internal/config"
 	"github.com/gofiber/fiber/v2"
