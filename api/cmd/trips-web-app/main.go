92301d260afc5cc563f7b1abcc0170ce9bcd9c4c
Laura Deng <laura@dimo.zone>
2024-03-07 22:18:34 -0600

    changed trips interface layout

--- api/cmd/trips-web-app/main.go
+++ api/cmd/trips-web-app/main.go
@@ -39,7 +39,7 @@ func main() {
 
 	fmt.Print("Server is starting...")
 
-	settings, err := shared.LoadConfig[config.Settings]("settings.yaml")
+	settings, err := shared.LoadConfig[config.Settings]("settings.prod.yaml")
 	if err != nil {
 		log.Fatal().Err(err).Msg("could not load settings")
 	}
