9109c8d13c0048733be0393bc601a13ac40e24c2
Laura Deng <laura@dimo.zone>
2025-01-27 13:00:28 -0500

    remove rainbowkit login and login with jwt

--- app-login/src/App.tsx
+++ app-login/src/App.tsx
@@ -1,29 +1,14 @@
-import { AuthenticationStatus, ConnectButton } from '@rainbow-me/rainbowkit';
 import './App.css';
-import { useAccount } from 'wagmi';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import {
-  RainbowKitProvider,
-  createAuthenticationAdapter,
-  RainbowKitAuthenticationProvider,
-} from '@rainbow-me/rainbowkit';
-import logo from './assets/whole_logo.png';
-import {
+  initializeDimoSDK,
   LoginWithDimo,
   ShareVehiclesWithDimo,
-  initializeDimoSDK,
   useDimoAuthState,
   DimoAuthProvider,
 } from '@dimo-network/login-with-dimo';
+import logo from './assets/whole_logo.png';
 
-class DIMODexMessage {
-  state?: string;
-  challenge?: string;
-  constructor(param: Partial<DIMODexMessage>) {
-    this.state = param.state;
-    this.challenge = param.challenge;
-  }
-}
 
 interface AuthData {
   token: string;
@@ -40,61 +25,6 @@ initializeDimoSDK({
 const permissionTemplateId = import.meta.env.DIMO_PERMISSION_TEMPLATE_ID;
 
 function App() {
-  const [status, setStatus] = useState<AuthenticationStatus>('unauthenticated');
-  const account = useAccount();
-
-  const authenticationAdapter = createAuthenticationAdapter({
-    getNonce: async () => {
-      const address = account?.address;
-      const response = await fetch(
-          `${import.meta.env.DIMO_API_BASEURL}/auth/web3/generate_challenge`,
-          {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ address }),
-          }
-      );
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch nonce');
-      }
-
-      const challengeResponse: string = await response.text();
-      return challengeResponse;
-    },
-
-    createMessage: ({ nonce }) => {
-      const message: { state: string; challenge: string } = JSON.parse(nonce);
-      return new DIMODexMessage({
-        state: message.state,
-        challenge: message.challenge,
-      });
-    },
-
-    verify: async ({ message, signature }) => {
-      const verifyRes = await fetch(
-          `${import.meta.env.DIMO_API_BASEURL}/auth/web3/submit_challenge`,
-          {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ state: message.state, signature }),
-          }
-      );
-
-      if (!verifyRes.ok) {
-        throw new Error('Failed to verify signature');
-      }
-
-      setStatus('authenticated');
-      return verifyRes.ok;
-    },
-
-    signOut: async () => {
-      await fetch('/api/logout');
-    },
-  });
-
-  // Use the Dimo authentication state
   const { isAuthenticated, getValidJWT } = useDimoAuthState();
 
   useEffect(() => {
@@ -120,7 +50,6 @@ function App() {
         .then((response) => {
           if (response.ok) {
             console.log('Session established, redirecting to /vehicles/me');
-            setStatus('authenticated');
           } else {
             console.error('Failed to establish session, response:', response);
           }
@@ -130,49 +59,36 @@ function App() {
         });
 
     window.location.href = `${import.meta.env.DIMO_API_BASEURL}/vehicles/me`;
-
   };
 
   return (
       <DimoAuthProvider>
-        <RainbowKitAuthenticationProvider adapter={authenticationAdapter} status={status}>
-          <RainbowKitProvider>
-            <>
-              <div className="logo-container">
-                <img src={logo} alt="Logo" className="logo" />
-              </div>
-              <div className="connect-button-container">
-                <ConnectButton />
-              </div>
-              <div className="connect-button-container">
-                <p>
-                  <a href="/login-jwt">Login with JWT</a>
-                </p>
-              </div>
-              <div className="connect-button-container">
-                {isAuthenticated ? (
-                    <ShareVehiclesWithDimo
-                        mode="popup"
-                        permissionTemplateId={permissionTemplateId}
-                        onSuccess={handleSuccess}
-                        onError={(error: Error) => {
-                          console.error('Error sharing vehicles:', error);
-                        }}
-                    />
-                ) : (
-                    <LoginWithDimo
-                        mode="popup"
-                        permissionTemplateId={permissionTemplateId}
-                        onSuccess={handleSuccess}
-                        onError={(error: Error) => {
-                          console.error('Authentication error:', error);
-                        }}
-                    />
-                )}
-              </div>
-            </>
-          </RainbowKitProvider>
-        </RainbowKitAuthenticationProvider>
+        <div className="app-container">
+          <div className="logo-container">
+            <img src={logo} alt="Logo" className="logo" />
+          </div>
+          <div className="login-button-container">
+            {isAuthenticated ? (
+                <ShareVehiclesWithDimo
+                    mode="popup"
+                    permissionTemplateId={permissionTemplateId}
+                    onSuccess={handleSuccess}
+                    onError={(error: Error) => {
+                      console.error('Error sharing vehicles:', error);
+                    }}
+                />
+            ) : (
+                <LoginWithDimo
+                    mode="popup"
+                    permissionTemplateId={permissionTemplateId}
+                    onSuccess={handleSuccess}
+                    onError={(error: Error) => {
+                      console.error('Authentication error:', error);
+                    }}
+                />
+            )}
+          </div>
+        </div>
       </DimoAuthProvider>
   );
 }
